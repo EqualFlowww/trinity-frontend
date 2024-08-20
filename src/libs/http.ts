0982bcd406d5e01b4e4900d571f16ad09b95f8da
JunggyeMessi <herrymh@naver.com>
2024-08-20 19:46:53 +0900

    test

--- src/libs/http.ts
+++ src/libs/http.ts
@@ -1,28 +1,49 @@
-// import { QueryClient } from '@tanstack/react-query';
+import { QueryClient } from '@tanstack/react-query';
 
-// export const queryClient = new QueryClient();
+export const queryClient = new QueryClient();
 
-// export async function fetchEvents({ signal, searchTerm }) {
-//   console.log(searchTerm);
-//   let url = 'http://localhost:3000/events';
+interface FetchCartParams {
+  signal?: AbortSignal;
+  searchTerm?: string;
+}
 
-//   if (searchTerm) {
-//     url += '?search=' + searchTerm;
-//   }
+export const fetchData = async (url: string, options?: RequestInit) => {
+  try {
+    const response = await fetch(url, options);
 
-//   const response = await fetch(url, { signal: signal });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
 
-//   if (!response.ok) {
-//     const error = new Error('An error occurred while fetching the events');
-//     error.code = response.status;
-//     error.info = await response.json();
-//     throw error;
-//   }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Fetch error: ', error);
+    throw error;
+  }
+};
 
-//   const { events } = await response.json();
+export async function fetchCarts({ signal }: FetchCartParams) {
+  let url = '/uerp/v1/demo/device/cart?$archive&$size=100';
 
-//   return events;
-// }
+  const data = await fetchData(url, {
+    signal: signal,
+    headers: window.common.auth.apiHeaders,
+  });
+
+  return data;
+}
+
+export async function fetchRounds({ signal }: FetchCartParams) {
+  let url = '/uerp/v1/demo/opertaion/round?$archive&$size=100';
+
+  const data = await fetchData(url, {
+    signal: signal,
+    headers: window.common.auth.apiHeaders,
+  });
+
+  return data;
+}
 
 // export async function createNewEvent(eventData) {
 //   const response = await fetch(`http://localhost:3000/events`, {
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,22 @@ import classes from './Home.module.scss';
 import Dashboard from '@/components/Home/Dashboard';
 import Overview from '@/components/Home/Overview';
 import { ActionFunctionArgs, LoaderFunctionArgs } from 'react-router-dom';
-// import { fetchEvents, queryClient } from '@/libs/http';
+import { fetchCarts, fetchRounds, queryClient } from '@/libs/http';
+import { useQuery } from '@tanstack/react-query';
 
 const Home = () => {
   const cx = classNames.bind(classes);
+  const {
+    data: cartData,
+    // isPending,
+    // error,
+  } = useQuery({
+    queryKey: ['carts'],
+    queryFn: ({ signal }) => fetchCarts({ signal }),
+  });
+
+  console.log(cartData);
+
   return (
     <div className={cx('main')}>
       <div className={cx('dashboard')}>
@@ -19,11 +31,15 @@ const Home = () => {
   );
 };
 
-const loader = ({}: LoaderFunctionArgs) => {
-  // queryClient.fetchQuery({
-  //   queryKey: ['home'],
-  //   queryFn: ({ signal }) => fetchEvents({ signal, searchTerm: '' }),
-  // });
+const loader = async ({}: LoaderFunctionArgs) => {
+  await queryClient.fetchQuery({
+    queryKey: ['carts'],
+    queryFn: ({ signal }) => fetchCarts({ signal }),
+  });
+  await queryClient.fetchQuery({
+    queryKey: ['rounds'],
+    queryFn: ({ signal }) => fetchRounds({ signal }),
+  });
   return null;
 };
 
