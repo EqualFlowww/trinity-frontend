17f9c07bf4d6ff85b746d168f6ad151c14051c04
jzide <jzide@JzWork>
2024-08-20 22:42:32 +0900

    update

--- webroot/static/js/main.js
+++ webroot/static/js/main.js
@@ -3,12 +3,14 @@
 var main = () => {
 	document.getElementById("eqpls-access-token").innerText = window.common.auth.accessToken;
 
+	/*
 	window.common.wsock.connect(
 		`/router/websocket?org=${window.common.auth.getOrg()}&token=${window.common.auth.accessToken}`,
 		(data) => {
 			console.log(data);
 		}
 	);
+	*/
 };
 
 window.common.init(() => {
