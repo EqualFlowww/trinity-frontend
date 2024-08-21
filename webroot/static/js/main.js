df1f16a28a435257681cbdf3579d92cc218c714d
jzide <jzide@JzWork>
2024-08-21 15:21:11 +0900

    update

--- webroot/static/js/main.js
+++ webroot/static/js/main.js
@@ -3,12 +3,17 @@
 var main = () => {
 	document.getElementById("eqpls-access-token").innerText = window.common.auth.accessToken;
 
-	window.common.wsock.connect(
-		`/router/websocket/cart/cc181cab-5dda-4b8c-aa3e-3cc9c55f5997?org=${window.common.auth.getOrg()}&token=${window.common.auth.accessToken}`,
-		(data) => {
-			console.log(data);
+	window.common.rest.get('/uerp/v1/demo/device/cart?$archive&$size=100', (carts) => {
+		console.log(carts);
+		if (carts && carts.length > 0) {
+			window.common.wsock.connect(
+				`/router/websocket/cart/${carts[0].id}?org=${window.common.auth.getOrg()}&token=${window.common.auth.accessToken}`,
+				(data) => {
+					console.log(data);
+				}
+			);
 		}
-	);
+	});
 };
 
 window.common.init(() => {
