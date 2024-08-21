098d91aee72ca94598063b279aaa0b24f8d05941
jzide <jzide@JzWork>
2024-08-21 14:41:02 +0900

    update

--- webroot/static/js/main.js
+++ webroot/static/js/main.js
@@ -3,14 +3,12 @@
 var main = () => {
 	document.getElementById("eqpls-access-token").innerText = window.common.auth.accessToken;
 
-	/*
 	window.common.wsock.connect(
-		`/router/websocket?org=${window.common.auth.getOrg()}&token=${window.common.auth.accessToken}`,
+		`/router/websocket/cart/d6da7219-c0e5-42f4-96dc-e45ebdefb463?org=${window.common.auth.getOrg()}&token=${window.common.auth.accessToken}`,
 		(data) => {
 			console.log(data);
 		}
 	);
-	*/
 };
 
 window.common.init(() => {
