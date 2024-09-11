ec467f67f40de5b16c1ecc389a5b0e5371732218
JunggyeMessi <herrymh@naver.com>
2024-09-11 17:01:23 +0900

    2024.09.11

--- src/components/UI/Scroll.tsx
+++ src/components/UI/Scroll.tsx
@@ -1,5 +1,11 @@
 import classNames from 'classnames/bind';
-import { useEffect, useRef, useState } from 'react';
+import {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react';
 import classes from './Scroll.module.scss';
 import getBrowser from '@/utils/getBrowser';
 
@@ -9,44 +15,53 @@ interface Props {
   size?: 'small' | 'medium' | 'large';
 }
 
-const Scroll = ({ children, type = 'vertical', size = 'medium' }: Props) => {
-  const cx = classNames.bind(classes);
-  const [broswer, setBroswer] = useState('');
+const Scroll = forwardRef<HTMLDivElement, Props>(
+  ({ children, type = 'vertical', size = 'medium' }, ref) => {
+    const cx = classNames.bind(classes);
+    const [broswer, setBroswer] = useState('');
 
-  const scrollContainerRef = useRef<HTMLDivElement>(null);
+    const scrollContainerRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const scrollContainer = scrollContainerRef.current;
-    setBroswer(getBrowser().toLowerCase());
-
-    if (!scrollContainer || type === 'vertical') {
-      return;
-    }
-
-    const onWheel = (event: WheelEvent) => {
-      if (event.deltaY !== 0) {
-        event.preventDefault();
-        scrollContainer.scrollLeft += event.deltaY;
+    useEffect(() => {
+      if (ref && scrollContainerRef.current) {
+        useImperativeHandle(ref, () => scrollContainerRef.current!);
       }
-    };
 
-    if (scrollContainer) {
-      scrollContainer.addEventListener('wheel', onWheel);
-    }
+      const scrollContainer = scrollContainerRef.current;
+      setBroswer(getBrowser().toLowerCase());
 
-    // Cleanup function
-    return () => {
+      if (!scrollContainer || type === 'vertical') {
+        return;
+      }
+
+      const onWheel = (event: WheelEvent) => {
+        if (event.deltaY !== 0) {
+          event.preventDefault();
+          scrollContainer.scrollLeft += event.deltaY;
+        }
+      };
+
       if (scrollContainer) {
-        scrollContainer.removeEventListener('wheel', onWheel);
+        scrollContainer.addEventListener('wheel', onWheel);
       }
-    };
-  }, []);
 
-  return (
-    <div className={cx('scroll', broswer, type, size)} ref={scrollContainerRef}>
-      <div className={cx('children-wrapper', type)}>{children}</div>
-    </div>
-  );
-};
+      // Cleanup function
+      return () => {
+        if (scrollContainer) {
+          scrollContainer.removeEventListener('wheel', onWheel);
+        }
+      };
+    }, []);
+
+    return (
+      <div
+        className={cx('scroll', broswer, type, size)}
+        ref={scrollContainerRef}
+      >
+        <div className={cx('children-wrapper', type)}>{children}</div>
+      </div>
+    );
+  }
+);
 
 export default Scroll;
