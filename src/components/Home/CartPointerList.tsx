b05aebed6950b017610253662612a125eea267df
mjk607 <mjk607@naver.com>
2024-09-13 17:37:38 +0900

    Modify

---
+++ src/components/Home/CartPointerList.tsx
@@ -0,0 +1,25 @@
+import CartPointer from '@/components/Home/CartPointer';
+import { CartSummaryDataCollection, RoundSummaryDataCollection } from '@/types/home';
+
+interface Props {
+  cartCollection: CartSummaryDataCollection;
+  roundCollection: RoundSummaryDataCollection;
+  calculateCartPosition: (cartLocation: { x: number; y: number }) => { x: number; y: number };
+}
+
+const CartPointerList = ({ cartCollection, roundCollection, calculateCartPosition }: Props) => {
+  return (
+    <>
+      {Object.values(cartCollection).map((cart) => (
+        <CartPointer
+          key={cart.id}
+          cart={cart}
+          round={cart.roundId ? roundCollection[cart.roundId] : undefined}
+          location={calculateCartPosition({ x: cart.location.x, y: cart.location.y })}
+        />
+      ))}
+    </>
+  );
+};
+
+export default CartPointerList;
